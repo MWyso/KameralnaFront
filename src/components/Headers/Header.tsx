61cb334e7a344175830d742f8275e0d6d5562cdc
MWyso <mateusz.wyso94@gmail.com>
2023-03-13 08:38:11 +0100

    Developing the Home component

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -2,10 +2,11 @@ import { NavHashLink } from 'react-router-hash-link';
 import styled from 'styled-components';
 import logo from '../../assets/images/logo3.png';
 import { useState } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import { useEventrixState } from 'eventrix';
 // import { BasketEntity } from 'types';
 import { PREFIX } from '../../config';
+
 
 
 
@@ -33,9 +34,27 @@ export const Header = () => {
                 <ul>
                     <NavHashLink
                         smooth
+                        to={PREFIX ? `${PREFIX}#about-us` : `/#about-us`}
+                        className="li">
+                        O nas
+                    </NavHashLink>
+                    <NavHashLink
+                        smooth
                         to={PREFIX ? `${PREFIX}#menu` : `/#menu`}
                         className="li">
                         Menu
+                    </NavHashLink>
+                    <NavHashLink
+                        smooth
+                        to={PREFIX ? `${PREFIX}#lunch` : `/#lunch`}
+                        className="li">
+                        Lunch
+                    </NavHashLink>
+                    <NavHashLink
+                        smooth
+                        to={PREFIX ? `${PREFIX}#find-us` : `/#find-us`}
+                        className="li">
+                        Znajdziesz nas
                     </NavHashLink>
                     <NavHashLink
                         smooth
@@ -44,6 +63,14 @@ export const Header = () => {
                         Kontakt
                     </NavHashLink>
                 </ul>
+                <div className="info-sm">
+                    <Link to="" className="top" title="Facebook">
+                        <i className="bx bxl-facebook-square" />
+                    </Link>
+                    <Link to="" className="top" title="Instagram">
+                        <i className="bx bxl-instagram-alt" />
+                    </Link>
+                </div>
             </div>
         </Container>
     );
@@ -72,16 +99,30 @@ const Container = styled.section<{ isActive: boolean }>`
   
     ul {
       display: flex;
-      z-index: 1;
+      text-decoration: none;
 
       .li {
         cursor: pointer;
         font-weight: 500;
         position: relative;
+        text-decoration: none;
       }
 
       .li:not(:last-child) {
         margin-right: 1rem;
       }
+
+      .info-sm {
+        width: 100%;
+        height: 4rem;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        font-size: ${(props) => props.theme.fontSize.lg};
+        z-index: 1;
+
+        .top {
+          z-index: 1;
+          cursor: pointer;
     }
 `;
